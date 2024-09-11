dbea4c64c1ed81c2b6c52cd2b8090f3cc3255bbc
Shailendra Kumar Rai <79188742+rishurai123@users.noreply.github.com>
2024-09-11 17:18:02 +0530

    Ignore essence directory

---
+++ mainApp/templatetags/customTags.py
@@ -0,0 +1,42 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='orderStatus')
+def orderStatus(Request,num):
+    if(num==0):
+        return "Order Placed"
+    elif(num==1):
+        return "Not Packed"
+    elif(num==2):
+        return "Packed"
+    elif(num==3):
+        return "Ready to Dispatch"
+    elif(num==4):
+        return "shipped"
+    elif(num==5):
+        return "out For delivery"
+    elif(num==6):
+        return "Delivered"
+    else:
+        return "cancelled"
+
+
+@register.filter(name='paymentStatus')
+def paymentStatus(Request,num):
+    if(num==0):
+        return "Pending"
+    else:
+        return "Done"
+
+@register.filter(name='paymentMode')
+def paymentMode(Request,num):
+    if(num==0):
+        return "COD"
+    else:
+        return "Net Banking"
+
+
+
+
+    
